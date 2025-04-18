c4a92f6b9ef22034a2f2af8a70881dfdea308266
Mark Hahl <mark.hahl@sigaint.au>
2025-04-18 20:10:18 +1000

    merge

--- config-parts/service_dhcp-server.sh
+++ config-parts/service_dhcp-server.sh
@@ -4,8 +4,6 @@ set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 optio
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 option domain-name 'sigaint.au'
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 subnet-id '1'
 
-
-
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 option name-server '45.90.28.189'
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 option name-server '45.90.30.189'
 
