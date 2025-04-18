b9955476c5ff4fbddb9d11a483de28a9591098e9
Mark Hahl <mark.hahl@sigaint.au>
2025-04-18 13:53:07 +1000

    change dhcp names

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -16,19 +16,12 @@ set interfaces ethernet eth1 offload sg
 set interfaces ethernet eth1 offload tso
 
 # Interfaces for WAN
-set interfaces pppoe pppoe0 description 'NET_NSW02_MGMT'
+set interfaces pppoe pppoe0 description 'NET_NSW02_WAN'
 set interfaces pppoe pppoe0 authentication password "$secret_ppoe_password"
 set interfaces pppoe pppoe0 authentication username "$secret_ppoe_username"
 set interfaces pppoe pppoe0 ip adjust-mss 'clamp-mss-to-pmtu'
 set interfaces pppoe pppoe0 mtu '1492'
 set interfaces pppoe pppoe0 source-interface 'eth0'
-
-set interfaces pppoe pppoe0 authentication password 'dodointernet'
-set interfaces pppoe pppoe0 authentication username 'philip.aubin@gmail.com'
-set interfaces pppoe pppoe0 ip adjust-mss 'clamp-mss-to-pmtu'
-set interfaces pppoe pppoe0 mtu '1492'
-set interfaces pppoe pppoe0 source-interface 'eth0'
-
 
 set interfaces ethernet pppoe0 dhcpv6-options pd 0 interface eth1 address '1'
 set interfaces ethernet pppoe0 dhcpv6-options pd 0 interface eth1 sla-id '0'
--- config-parts/service_dhcp-server.sh
+++ config-parts/service_dhcp-server.sh
@@ -9,17 +9,17 @@ set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 optio
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 range 0 start '10.130.0.100'
 set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 range 0 stop '10.130.0.200'
 
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping BLUEIRIS ip-address '10.130.1.3'
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping BLUEIRIS mac 'AC:E2:D3:0B:7A:60'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping blueiris ip-address '10.130.1.3'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping blueiris mac 'AC:E2:D3:0B:7A:60'
 
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping JELLYFIN ip-address '10.130.1.4'
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping JELLYFIN mac '10:62:e5:00:9f:55'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping plex ip-address '10.130.1.4'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping plex mac '10:62:e5:00:9f:55'
 
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping QNAP_1A ip-address '10.130.1.1'
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping QNAP_1A mac '00:08:9B:F1:D1:82'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping qnap-1a ip-address '10.130.1.1'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping qnap-1a mac '00:08:9B:F1:D1:82'
 
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping TESLA_CHARGER ip-address '10.130.1.2'
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping TESLA_CHARGER mac '98:ED:5C:CA:CC:60'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping tesla-charger  ip-address '10.130.1.2'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping tesla-charger mac '98:ED:5C:CA:CC:60'
 
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping TL_SG116E ip-address '10.130.1.250'
-set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping TL_SG116E mac 'f0:09:0d:78:1e:c1'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping tplink-sw1 ip-address '10.130.1.250'
+set service dhcp-server shared-network-name NEWCASTLE subnet 10.130.0.0/16 static-mapping tplink-sw1 mac 'f0:09:0d:78:1e:c1'
