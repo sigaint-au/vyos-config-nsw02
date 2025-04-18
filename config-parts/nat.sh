d843418c3b44e7689e7724f5fbfb2803ff919605
Mark Hahl <mark.hahl@sigaint.au>
2025-04-18 14:34:24 +1000

    fix typo

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -2,6 +2,6 @@
 
 ## Define NAT rules
 #
-set nat source rule 50 outbound-interface name 'ppoe0'
+set nat source rule 50 outbound-interface name 'pppoe0'
 set nat source rule 50 source address '10.130.0.0/16'
 set nat source rule 50 translation address 'masquerade'
